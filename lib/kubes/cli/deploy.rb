dcec52088689ac7dd5707115123ebce861c7f7b0
Tung Nguyen <tongueroo@gmail.com>
2020-11-02 20:04:10 +0000

    write full.yaml also

--- lib/kubes/cli/deploy.rb
+++ lib/kubes/cli/deploy.rb
@@ -1,7 +1,7 @@
 class Kubes::CLI
   class Deploy < Base
     def run
-      Build.new(@options).run if build?
+      Build.new(@options).run
       Apply.new(@options).run # also calls Compile
     end
   end
--- lib/kubes/kubectl/batch.rb
+++ lib/kubes/kubectl/batch.rb
@@ -49,38 +49,5 @@ class Kubes::Kubectl
       end
       result
     end
-
-    # kubes apply                   # {role: nil, resource: nil}
-    # kubes apply clock             # {role: "clock", resource: nil}
-    # kubes apply clock deployment  # {role: "clock", resource: "deployment"}
-    def search_expr
-      role, resource = @options[:role], @options[:resource]
-      if role && resource
-        "#{Kubes.root}/.kubes/output/#{role}/#{resource}.yaml"
-      elsif role
-        "#{Kubes.root}/.kubes/output/#{role}/*.yaml"
-      else
-        "#{Kubes.root}/.kubes/output/**/*.yaml"
-      end
-    end
-
-    def files
-      files = []
-      Dir.glob(search_expr).each do |path|
-        next unless process?(path)
-        file = path.sub("#{Kubes.root}/", '')
-        files << file
-      end
-      files
-    end
-
-    def process?(path)
-      consider?(path) && two_levels_deep?(path)
-    end
-
-    def two_levels_deep?(path)
-      rel_path = path.sub(%r{.*\.kubes/output/},'')
-      rel_path.split('/').size == 2
-    end
   end
 end
