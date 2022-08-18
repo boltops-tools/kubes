9935338b6a4f5bf2bfaec07d13a646e0961583c1
Tung Nguyen <tongueroo@gmail.com>
2022-08-18 03:33:12 +0000

    logger debug rendering path

--- lib/kubes/compiler/strategy/dispatcher.rb
+++ lib/kubes/compiler/strategy/dispatcher.rb
@@ -16,6 +16,8 @@ class Kubes::Compiler::Strategy
         klass = dsl_class(path) # IE: Kubes::Compiler::Dsl::Syntax::Deployment or Kubes::Compiler::Dsl::Core::Blocks
         klass.new(@options.merge(path: path, data: @data)).run
       else
+        pretty_path = path.sub("#{Kubes.root}/",'')
+        logger.debug "Rendering #{pretty_path}"
         Erb.new(@options.merge(data: @data)).render_result(path)
       end
     end
