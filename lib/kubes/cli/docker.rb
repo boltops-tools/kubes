ce0b5c561465dd290387c41a12fb0e32692bbd7e
Tung Nguyen <tongueroo@gmail.com>
2022-10-10 19:07:21 +0000

    kubes docker name

--- lib/kubes/cli/docker.rb
+++ lib/kubes/cli/docker.rb
@@ -9,7 +9,25 @@ class Kubes::CLI
       push if options[:push]
     end
 
-    desc "push IMAGE", "Push the docker image."
+    desc "name", "Print the full docker image with tag that was last generated."
+    long_desc Help.text("docker:name")
+    option :name, type: :boolean, default: false
+    def name
+      builder = Kubes::Docker.new(options, "build")
+      name = builder.read_image_name
+      if name
+        puts name
+      else
+        $stderr.puts(<<~EOL)
+          WARN: docker image has not yet been built. Please first run:
+
+              kubes docker build
+
+        EOL
+      end
+    end
+
+    desc "push", "Push the docker image."
     long_desc Help.text("docker:push")
     option :push, type: :boolean, default: false
     def push
